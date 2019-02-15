7ca5957f345a85e08520ecd695437a756ddd3a43
diego <diegovalle@gmail.com>
2019-02-14 21:02:11 -0600

    models for January 19 data

--- run-all.R
+++ run-all.R
@@ -4,3 +4,10 @@ source(file.path("src", "clean-pgj.R"))
 # Clean SESNSP data
 lastGood = as.Date(as.yearmon("2019-01-01"), frac = 1)
 source(file.path("src", "pgj.R"))
+
+
+start_date <-  "2018-02-01"
+end_date <- "2019-02-01"
+
+source(file.path("src", "gam.R"))
+source(file.path("src", "pointdensity.R"))
--- src/gam.R
+++ src/gam.R
@@ -1,7 +1,6 @@
 
-
 hom_last_year <- read.csv("clean-data/crime-lat-long-pgj.csv", stringsAsFactors = FALSE) %>%
-  filter(date <= "2018-12-31" & date >= "2018-01-01") %>%
+  filter(date >=  start_date & date < end_date) %>%
   filter(crime == "HOMICIDIO DOLOSO") %>%
   group_by(cuadrante) %>%
   summarise(count = n()) %>%
@@ -29,34 +28,12 @@ df$SUMPOB1 <- as.integer(df$SUMPOB1)
 # No cuadrantes with zero population, (besides it doensn't happen IRL)
 df$SUMPOB1[df$SUMPOB1 == 0 ] <- 100 
 
-
-#Some cuadrantes contain hospitals
-# df$hospital <- FALSE
-# df$hospital[df$cuadrante %in% c( "N-4.4.4", "C-2.1.16", "N-2.2.1", "O-2.5.7", 
-#                                  "O-2.2.4", "N-1.3.10", "P-1.5.7", "P-3.1.1")] <- TRUE
-# df$hospital <- as.factor(df$hospital)
-# 
-# df$municipio <- as.factor(df$municipio)
-# 
-# cuad.nb <- knn2nb(knearneigh(coordinates(cuad_map), k = 16), row.names = df$id)
-# names(cuad.nb) <- attr(cuad.nb, "region.id")
-# 
-# ll <- lapply(cuad.nb, function(x) x)
-# names(ll) <- as.character(cuadrantes$id)
-# 
-# hom$smooth <- sapply(1:nrow(hom), function(x) {
-#   w <-  c(hom$population[x], hom$population[cuad.nb[[x]]])
-#   r <- c(hom$rate[x], hom$rate[cuad.nb[[x]]])
-#   return(sum(w * r)/sum(w))
-# })
-
-
 # Zero inflated?
 ggplot(df, aes(count)) +
-  geom_histogram(bins = 35)
+  geom_histogram()
 
 ctrl <- gam.control(nthreads = 4)
-m1 <- gam(count ~ s(id, bs = 'mrf', k = 840, xt = list(nb = nb)) + offset(log(SUMPOB1)) + Sector_hoy,
+m1 <- gam(count ~ s(id, bs = 'mrf', k = 710, xt = list(nb = nb)) + offset(log(SUMPOB1)) + Sector_hoy,
           data = df,
           method = 'REML',
           family = tw
@@ -70,6 +47,8 @@ m1 <- gam(count ~ s(id, bs = 'mrf', k = 840, xt = list(nb = nb)) + offset(log(SU
 #summary(m2)
 #anova(m1, m2)
 #plot(m1, select=3)
+#save(m1, file = "clean-data/m1.RData")
+#load("clean-data/m1.RData")
 
 df$resid.gam.mod <- residuals(m1, type = "pearson")
 df$fit.gam.mod <- residuals(m1, type = "pearson")
@@ -88,13 +67,16 @@ df$rate <- df$count / df$SUMPOB1 * 10^5
 labels <- data.frame(
   name = c("Valle de San Lorenzo", "Tepito", " San Felipe de Jesús", 
            "Cerro del Chiquihuite", "Bellavista", "Ermita Zaragoza",
-           "Central de Abastos", "San Andrés Tomatlán", "Topilejo"),
+           "Central de Abastos", "San Andrés Tomatlán", "Topilejo",
+           "Santa María Aztahuacan"),
   lat =c(19.299767, 19.445793, 19.496768, 
          19.542162, 19.397080, 19.367161,
-         19.373099, 19.327635, 19.199720),
+         19.373099, 19.327635, 19.199720,
+         19.347272),
   long = c(-99.078852, -99.128877, -99.075112, 
            -99.134397, -99.193402, -98.999505, 
-           -99.091441, -99.103074, -99.143017),
+           -99.091441, -99.103074, -99.143017, 
+           -99.021737),
   group = NA
 )
 
@@ -106,7 +88,7 @@ ggplot(mdata, aes(x = long, y = lat, group = group)) +
   geom_path(col = 'black', alpha = 0.5, size = 0.05) +
   coord_map() +
   geom_label_repel(data = labels, aes(long, lat, label = name), size = 3,
-                   force = .6, alpha = .8,
+                   force = .8, alpha = .8,
                    box.padding = 3.3, label.padding = 0.18) +
   scale_fill_viridis(name = "rate", 
                      #limits = c(0, 120),
@@ -130,7 +112,7 @@ ggplot(mdata, aes(x = long, y = lat, group = group)) +
         panel.grid.minor=element_blank(),
         #panel.spacing=unit(0, "lines"),
         plot.background=element_blank())+
-  ggtitle(str_c("Modeled Homicide Rates in Mexico City (Jan 2018 - Dec 2018)"),
+  ggtitle(str_c("Modeled Homicide Rates in Mexico City (Feb 2018 - Jan 2018)"),
           subtitle = str_c("Because some cuadrantes have a low population and homicides tend to be rare occurrences\n",
                            "the variance in homicide rates per 100,000 tends to be high. To remove some of the variance,\n",
                            "and help discover patterns in the data, the homicide rate in each cuadrante was calculated\n",
