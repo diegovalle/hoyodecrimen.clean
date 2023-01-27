e4133072091c95920e093ebab1950b9fc31d63d1
diego <git@diegovalle.net>
2023-01-27 12:47:53 -0600

    Update database with a GitHub Action

---
+++ update/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#  ./update.sh postgres://deploy:xxxxx@hoyodecrimen.com:44765/apihoyodecrimen xxxx
+
+set -euo pipefail #exit on error, undefined and prevent pipeline errors
+IFS=$'\n\t'
+URL="https://data.diegovalle.net/hoyodecrimen"
+
+wget -N $URL/crime-lat-long-pgj.csv.zip $URL/cuadrantes-pgj.csv.zip $URL/pgj.csv.zip -P "$TMPDIR"
+(cd "$TMPDIR" && unzip -o -d "$TMPDIR/$(date +%Y-%m)" -j '*csv.zip')
+cp "$TMPDIR"/"$(date +%Y-%m)"/*.csv "/tmp"
+psql -d "$1" -f "$GITHUB_WORKSPACE"/update/update.sql
+# psql -d apihoyodecrimen -f update.sql
+
+psql -d "$1" -c "VACUUM ANALYZE crime_latlong;"
+psql -d "$1" -c "VACUUM ANALYZE cuadrantes;"
+psql -d "$1" -c "VACUUM ANALYZE pgj;"
+
+curl -X POST -d "CACHE_SECRET=$2" https://hoyodecrimen.com/clear-cache
+xargs -I % wget -q --show-progress --wait=14 --tries=3 -O /dev/null https://hoyodecrimen.com% < "$GITHUB_WORKSPACE"/update/urllist.txt
