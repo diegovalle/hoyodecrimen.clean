43ed9db4c96dee1d4358d7d7bd30ad2622110588
diego <git@diegovalle.net>
2023-10-27 17:07:53 -0600

    print

--- src/clean-pgj.R
+++ src/clean-pgj.R
@@ -303,16 +303,19 @@ expect_true(all(str_detect(df$fecha_hechos,
 #df$fecha_hechos <- if_else(str_length(df$fecha_hechos) == 16, 
 #        paste0(df$fecha_hechos, ":00"), 
 #        df$fecha_hechos)
-
+print("parse_date_time(df$fecha_hechos, ")
 df$fecha_hechos2 <- parse_date_time(df$fecha_hechos, "Ymd H:M:S")
 
+print("format(df$fecha_hechos2")
 expect_true(all(str_detect(
   format(df$fecha_hechos2, "%Y-%m-%d %H:%M:%S"),
   "\\d{4}-\\d{2}-\\d{2} \\d{1,2}:\\d{2}:\\d{2}")))
 expect_true(max(df$fecha_hechos2) < Sys.Date())
 
+print("year(df$fecha_hechos2)")
 expect_identical(df$Año, year(df$fecha_hechos2))
 df <- filter(df, Mes != "data")
+print("month(df$fecha_hechos2")
 expect_equal(as.character(month(df$fecha_hechos2, label = TRUE, abbr = TRUE)),
              str_replace_all(df$Mes,
                              c("Enero" = "Jan", "Febrero" = "Feb", "Marzo" = "Mar",
@@ -321,6 +324,7 @@ expect_equal(as.character(month(df$fecha_hechos2, label = TRUE, abbr = TRUE)),
                                "Octubre" = "Oct", "Noviembre" = "Nov", "Diciembre" = "Dec"))
 )
 
+print(" format(as.Date(df$fecha_hechos2), ")
 df$Mes.y.año <- format(as.Date(df$fecha_hechos2), "%Y-%m")
 expect_true(all(str_detect(df$Mes.y.año, "\\d{4}-\\d{2}")))
 
